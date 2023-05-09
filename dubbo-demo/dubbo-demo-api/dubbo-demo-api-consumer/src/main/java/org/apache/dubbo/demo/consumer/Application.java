edbfe52e35cc2de45a10bf856cf94f6c9a949fe2
604138 <1010024603@qq.com>
2023-05-09 14:48:19 +0800

    api demo zookeeper

--- dubbo-demo/dubbo-demo-api/dubbo-demo-api-consumer/src/main/java/org/apache/dubbo/demo/consumer/Application.java
+++ dubbo-demo/dubbo-demo-api/dubbo-demo-api-consumer/src/main/java/org/apache/dubbo/demo/consumer/Application.java
@@ -19,22 +19,65 @@
 package org.apache.dubbo.demo.consumer;
 
 import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.MethodConfig;
 import org.apache.dubbo.config.ReferenceConfig;
 import org.apache.dubbo.config.RegistryConfig;
 import org.apache.dubbo.demo.DemoService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     /**
      * In order to make sure multicast registry works, need to specify '-Djava.net.preferIPv4Stack=true' before
      * launch the application
      */
+//    public static void main(String[] args) {
+//        ReferenceConfig<DemoService> reference = new ReferenceConfig<>();
+//        reference.setApplication(new ApplicationConfig("dubbo-demo-api-consumer"));
+//        reference.setRegistry(new RegistryConfig("multicast://224.5.6.7:1234"));
+//        reference.setInterface(DemoService.class);
+//        DemoService service = reference.get();
+//        String message = service.sayHello("dubbo");
+//        System.out.println(message);
+//    }
+
     public static void main(String[] args) {
-        ReferenceConfig<DemoService> reference = new ReferenceConfig<>();
-        reference.setApplication(new ApplicationConfig("dubbo-demo-api-consumer"));
-        reference.setRegistry(new RegistryConfig("multicast://224.5.6.7:1234"));
+        // 当前应用配置
+        ApplicationConfig application = new ApplicationConfig();
+        application.setName("xxx");
+
+        // 连接注册中心配置
+        RegistryConfig registry = new RegistryConfig();
+        registry.setAddress("10.20.130.230:9090");
+        registry.setUsername("aaa");
+        registry.setPassword("bbb");
+
+        // 注意：ReferenceConfig为重对象，内部封装了与注册中心的连接，以及与服务提供方的连接
+
+        // 方法级配置
+        List<MethodConfig> methods = new ArrayList<MethodConfig>();
+        MethodConfig method = new MethodConfig();
+        method.setName("createXxx");
+        method.setTimeout(10000);
+        method.setRetries(0);
+        methods.add(method);
+
+        // 引用远程服务
+        ReferenceConfig<DemoService> reference = new ReferenceConfig<DemoService>(); // 此实例很重，封装了与注册中心的连接以及与提供者的连接，请自行缓存，否则可能造成内存和连接泄漏
+        reference.setApplication(application);
+        reference.setRegistry(registry); // 多个注册中心可以用setRegistries()
         reference.setInterface(DemoService.class);
-        DemoService service = reference.get();
-        String message = service.sayHello("dubbo");
-        System.out.println(message);
+        reference.setVersion("1.0.0");
+
+//        reference.setMethods(methods); // 设置方法级配置
+
+        // 如果点对点直连，可以用reference.setUrl()指定目标地址，设置url后将绕过注册中心，
+// 其中，协议对应provider.setProtocol()的值，端口对应provider.setPort()的值，
+// 路径对应service.setPath()的值，如果未设置path，缺省path为接口名
+//        reference.setUrl("dubbo://10.20.130.230:20880/com.xxx.XxxService");
+
+        // 和本地bean一样使用xxxService
+        DemoService demoService = reference.get(); // 注意：此代理对象内部封装了所有通讯细节，对象较重，请缓存复用
     }
 }
--- dubbo-demo/dubbo-demo-api/dubbo-demo-api-provider/src/main/java/org/apache/dubbo/demo/provider/Application.java
+++ dubbo-demo/dubbo-demo-api/dubbo-demo-api-provider/src/main/java/org/apache/dubbo/demo/provider/Application.java
@@ -19,6 +19,7 @@
 package org.apache.dubbo.demo.provider;
 
 import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ProtocolConfig;
 import org.apache.dubbo.config.RegistryConfig;
 import org.apache.dubbo.config.ServiceConfig;
 import org.apache.dubbo.demo.DemoService;
@@ -28,13 +29,46 @@ public class Application {
      * In order to make sure multicast registry works, need to specify '-Djava.net.preferIPv4Stack=true' before
      * launch the application
      */
+//    public static void main(String[] args) throws Exception {
+//        ServiceConfig<DemoServiceImpl> service = new ServiceConfig<>();
+//        service.setApplication(new ApplicationConfig("dubbo-demo-api-provider"));
+//        service.setRegistry(new RegistryConfig("multicast://224.5.6.7:1234"));
+//        service.setInterface(DemoService.class);
+//        service.setRef(new DemoServiceImpl());
+//        service.export();
+//        System.in.read();
+//    }
+
     public static void main(String[] args) throws Exception {
-        ServiceConfig<DemoServiceImpl> service = new ServiceConfig<>();
-        service.setApplication(new ApplicationConfig("dubbo-demo-api-provider"));
-        service.setRegistry(new RegistryConfig("multicast://224.5.6.7:1234"));
+        // 服务实现
+        DemoService demoService = new DemoServiceImpl();
+
+        //当前应用配置
+        ApplicationConfig application = new ApplicationConfig();
+        application.setName("xxx");
+
+        //连接注册中心
+        RegistryConfig registry = new RegistryConfig();
+        registry.setAddress("zookeeper://zookeeper.sirius.com:32181");
+
+        // 服务提供者协议配置
+        ProtocolConfig protocol = new ProtocolConfig();
+        protocol.setName("dubbo");
+        protocol.setPort(12345);
+        protocol.setThreads(200);
+
+        // 注意：ServiceConfig为重对象，内部封装了与注册中心的连接，以及开启服务端口
+
+        // 服务提供者暴露服务配置
+        ServiceConfig<DemoService> service = new ServiceConfig<DemoService>(); // 此实例很重，封装了与注册中心的连接，请自行缓存，否则可能造成内存和连接泄漏
+        service.setApplication(application);
+        service.setRegistry(registry); // 多个注册中心可以用setRegistries()
+        service.setProtocol(protocol); // 多个协议可以用setProtocols()
         service.setInterface(DemoService.class);
-        service.setRef(new DemoServiceImpl());
+        service.setRef(demoService);
+        service.setVersion("1.0.0");
+
+// 暴露及注册服务
         service.export();
-        System.in.read();
     }
 }
